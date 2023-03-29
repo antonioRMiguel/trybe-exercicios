d9f7c1db149d723a542f32bfaa9460bfe9774897
antonio <arm87325@gmail.com>
2023-03-28 23:22:40 -0300

    feito o exercicio dos números pares

---
+++ a-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/numPar.js
@@ -0,0 +1,11 @@
+// 8 - Defina três números em variáveis diferentes e retorne true se uma das três for par. Caso contrário, ele retorna false.
+
+let num1 = 17;
+let num2 = 77;
+let num3 = 7;
+
+if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+    console.log(true);
+} else {
+    console.log(false);
+} 
