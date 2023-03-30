116f837c5390639e92810ca01b4fa246678f3156
antonio <arm87325@gmail.com>
2023-03-30 15:45:52 -0300

    criado exrcicio da sorveteria

---
+++ a-fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/sorveteria.js
@@ -0,0 +1,36 @@
+let clienteCadastrado = ["Marta", "Paulo", "Maria", "Nuria", "Manoela", "Fernanda"];
+let saboresSorvetes = ["morango", "pistache", "chocolate", "baunilha", "coco", "limão"];
+
+let clienteValido = false;
+let saborValido = false;
+
+for (let index = 0; index < clienteCadastrado.length; index += 1) {
+    if (clienteCadastrado[index] === "Paulo") {
+        clienteValido = true;
+    }
+}
+
+for (let index = 0; index < saboresSorvetes.length; index += 1) {
+    if (saboresSorvetes[index] === "chocolate") {
+        saborValido = true;
+    }
+}
+
+if (clienteValido === true && saborValido === true) {
+    console.log("Olá, Paulo. Aqui está seu sorvete de chocolate.");
+}
+
+if (clienteValido === false) {
+    console.log("Olá, Paulo. Você não está cadastrado em nosso sistema.");
+}
+
+if (saborValido === false) {
+    console.log("Olá, Paulo. O sabor chocolate não está disponível.");
+}
+
+if (clienteValido === false && saborValido === false) {  
+    console.log("Olá, Paulo. Você não está cadastrado em nosso sistema e o sabor chocolate não está disponível.");
+}
+
+
+
