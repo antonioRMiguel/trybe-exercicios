19bb349a26a5a13cc9a62e08a8dd920be5472309
antonio <arm87325@gmail.com>
2023-03-30 15:23:05 -0300

    cria os exercicios do dia 03

---
+++ a-fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/exercicio-exemplo-cumprimenta-cliente.js
@@ -0,0 +1,6 @@
+function cumprimentaCliente(cliente) {
+  return 'Olá, ' + cliente + '. Essa é sua conta do TrybeBank'
+}
+
+console.log(cumprimentaCliente('Isaac')) // Olá, Isaac. Essa é sua conta do TrybeBank
+
