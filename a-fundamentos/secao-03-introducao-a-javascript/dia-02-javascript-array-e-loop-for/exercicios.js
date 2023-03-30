7be3c2a4881055a707645161bffd9f25086f6580
antonio <arm87325@gmail.com>
2023-03-30 07:49:50 -0300

    adiciona o arquivo exercícios .jd já com arrays de nome e número

---
+++ a-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios.js
@@ -0,0 +1,16 @@
+// Utilize o for/of para imprimir os elementos da lista names com o console.log():
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let index of names){
+    console.log(index);
+}
+
+
+// Mostra o números do array
+
+let numeros = [1, 2 , 3, 4, 5];
+
+for(let index of numeros) { 
+    console.log(index);
+}
