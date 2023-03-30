6eab59305fb6b042cbab7841295385a810603abc
antonio <arm87325@gmail.com>
2023-03-30 16:15:56 -0300

    subindo arquivos agora, pois luz está oscilando

--- a-fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/funcaoDaSorveteria.js
+++ a-fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/funcaoDaSorveteria.js
@@ -1,38 +1,28 @@
 let clienteCadastrado = ["Marta", "Paulo", "Maria", "Nuria", "Manoela", "Fernanda"];
 let saboresSorvetes = ["morango", "pistache", "chocolate", "baunilha", "coco", "limão"];
 
-function pedidoSorvete() {
+function pedidoSorvete(nome, sabor) {
     let clienteValido = false;
     let saborValido = false;
 
     for (let index = 0; index < clienteCadastrado.length; index += 1) {
-        if (clienteCadastrado[index] === "Paulo") {
+        if (clienteCadastrado[index] === nome) {
             clienteValido = true;
         }
     }
 
     for (let index = 0; index < saboresSorvetes.length; index += 1) {
-        if (saboresSorvetes[index] === "chocolate") {
+        if (saboresSorvetes[index] === sabor) {
             saborValido = true;
         }
     }
 
     if (clienteValido === true && saborValido === true) {
-        console.log("Olá, Paulo. Aqui está seu sorvete de chocolate.");
-    }
-
-    if (clienteValido === false) {
-        console.log("Olá, Paulo. Você não está cadastrado em nosso sistema.");
-    }
-
-    if (saborValido === false) {
-        console.log("Olá, Paulo. O sabor chocolate não está disponível.");
-    }
-
-    if (clienteValido === false && saborValido === false) {  
-        console.log("Olá, Paulo. Você não está cadastrado em nosso sistema e o sabor chocolate não está disponível.");
+        console.log("nome + ", pague seu sorvete de " + sabor + " no caixa."");
+    } else {
+        console.log("CLiente e/ou sorvete não cadastrado")
     }
 }
 
-pedidoSorvete();
+pedidoSorvete(Maria, pistache);
 
