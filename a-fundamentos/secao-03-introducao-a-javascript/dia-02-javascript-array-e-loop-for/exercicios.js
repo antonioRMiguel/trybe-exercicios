12d8765268af1e7b25843c820e993a0fe4880087
antonio <arm87325@gmail.com>
2023-03-30 08:06:52 -0300

    adiciona exercicio de while com jogo de dados

--- a-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios.js
+++ a-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios.js
@@ -14,3 +14,16 @@ let numeros = [1, 2 , 3, 4, 5];
 for(let index of numeros) { 
     console.log(index);
 }
+
+// Ao contrário do for, o while executa a iteração com apenas uma condição. 
+// Enquanto essa condição for verdadeira, o conteúdo de escopo do while vai sendo cumprido e, ao encontrar uma condição falsa, o bloco é encerrado. 
+// Por exemplo:
+// obs: o while é mais utilizado quando não sabemos o número de iterações que precisamos fazer.
+// obs2: Os códigos de número devolvem ao console o número de iterações que foram feitas. São códigos com o mesmo resulta!
+
+let counter = 0;
+
+while(counter !== 5) {
+  counter += 1;
+  console.log(counter);
+}
