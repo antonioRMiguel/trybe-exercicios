28adc331c56cc491eb8c6f128f864b5763b1765b
Antônio Rodrigues Miguel <arm87325@gmail.com>
2023-05-01 01:50:01 -0300

    solução parte 3

--- a-fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-let-const-arrowfunctions-templatesliterais/script.js
+++ a-fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-let-const-arrowfunctions-templatesliterais/script.js
@@ -17,6 +17,23 @@ imprimeIdade()
   console.log('Nome:', pessoa.nome);
   console.log('Idade:', pessoa.idade);
 
+  
   let favoriteFood = 'Lasanha';
   favoriteFood = 'Hambúrguer';
   console.log(favoriteFood);
+
+
+  const name = 'Adriana';
+  const lastName = 'Soares';
+
+  console.log(`Olá, ${name} ${lastName}!`);
+  
+  function soma(a,b) {
+    let resultado = a + b;
+    return resultado;
+  }
+
+  let a = 3;
+  let b = 5;
+
+  console.log(`O resultado da soma de: ${a} + ${b} é: ${soma(a,b)}`);  
